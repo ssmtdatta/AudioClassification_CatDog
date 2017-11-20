f8dd7ea17081436473157fde1213b7889d48710f
Susmita Datta <ssmtdatta@gmail.com>
2017-11-19 23:31:32 -0600

    exploratory notebooks

---
+++ SupportModules/par_dirpath.py
@@ -0,0 +1,13 @@
+
+BASE_DIR = "/Users/susmitadatta/Projects/AudioClassification_CatDog/"
+
+DATA_DIR = BASE_DIR+"Data_cats_dogs/"
+
+CODE_DIR = BASE_DIR+"Codes/"
+
+SUPMOD_DIR = BASE_DIR+"SupportModules/"
+
+TRAIN_CAT_DIR = DATA_DIR+"Train/Cat/"
+TRAIN_DOG_DIR = DATA_DIR+"Train/Dog/"
+TEST_CAT_DIR = DATA_DIR+"Test/Cat/"
+TEST_DOG_DIR = DATA_DIR+"Test/Dog/"
